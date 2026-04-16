06013b4839f7de7164db2664b707fd663178dab9
sofiyabo <161760235+sofiyabo@users.noreply.github.com>
2026-04-16 15:07:02 -0300

    entregar

--- src/utils.py
+++ src/utils.py
@@ -116,21 +116,7 @@ def plot_school_analysis(df, feats_numericas, label_rend, label_sem):
     plt.tight_layout()
     plt.show()
 
-"""
-def cv(folds, model_fn, preprocess_fn, metrics_fn):
-    all_metrics = []
 
-    for fold in folds:
-        df_train, df_val = fold
-
-        X_train, y_train, X_val, y_val = preprocess_fn(df_train, df_val)
-        model   = model_fn(X_train, y_train)
-        metrics = metrics_fn(model, X_val, y_val)
-        all_metrics.append(metrics)
-
-    return all_metrics
-"""
-    
 def matrices(df, feature_cols, target):
     
     X = df[feature_cols].values
